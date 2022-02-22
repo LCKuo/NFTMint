830fe4de4b779edf9bab544fbe1ab218bc270922
LCKuo <asdfg6311@gmail.com>
2022-02-22 13:02:16 +0800

    Updates

---
+++ precache-manifest.9a40bf80e3db1af9ed8f92acdcf1c5a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8807afd47c0f35eb8a89",
+    "url": "/NFTMint/static/css/main.b92a27a2.chunk.css"
+  },
+  {
+    "revision": "8807afd47c0f35eb8a89",
+    "url": "/NFTMint/static/js/main.8807afd4.chunk.js"
+  },
+  {
+    "revision": "5b2157dbc28c9da5d7cf",
+    "url": "/NFTMint/static/css/1.8de96408.chunk.css"
+  },
+  {
+    "revision": "5b2157dbc28c9da5d7cf",
+    "url": "/NFTMint/static/js/1.5b2157db.chunk.js"
+  },
+  {
+    "revision": "418e8df91fe5c331baf5",
+    "url": "/NFTMint/static/js/runtime~main.418e8df9.js"
+  },
+  {
+    "revision": "9697bb334465e28ca8b9f9dbefec2a96",
+    "url": "/NFTMint/static/media/LC.9697bb33.png"
+  },
+  {
+    "revision": "76e673b3098681856cf86daa73031ce9",
+    "url": "/NFTMint/static/media/eth.76e673b3.png"
+  },
+  {
+    "revision": "992fa27d1648e924d2cfbf958e8f0f3e",
+    "url": "/NFTMint/static/media/logo.992fa27d.png"
+  },
+  {
+    "revision": "b32f2ee3d465eb811a56793c95805583",
+    "url": "/NFTMint/index.html"
+  }
+];
